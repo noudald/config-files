2c642d27a1ae18cd144637e634e1e01758b93360
Noud Aldenhoven <noud.aldenhoven@gmail.com>
2018-03-18 11:38:09 +0100

    Backup file permissions changed.

--- backup.zsh
+++ backup.zsh
@@ -8,9 +8,9 @@ echo ${backup_folder}
 mkdir -p ${backup_folder}
 
 rsync --archive --verbose --itemize-changes \
-	--link-dest=${backup_main_folder}/current \
-       	${HOME}/{documents,work,nowork,Dropbox} ${backup_folder}/ \
-	| tee ${backup_main_folder}/backup-${backup_name}.log
+    --link-dest=${backup_main_folder}/current \
+    /home/noud/{documents,work,nowork,Dropbox} ${backup_folder}/ \
+    | tee ${backup_main_folder}/backup-${backup_name}.log
 
 chown noud:noud ${backup_main_folder}/backup-${backup_name}.log
 chown -R noud:noud ${backup_folder}
